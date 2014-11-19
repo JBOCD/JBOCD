17478df1f5e59ef029a962080a60a999acd4f456
Allen Poon <a1b2c3d42003hongkong@gmail.com>
2014-11-19 14:15:55 +0800

    fixed bug

--- server/MySQL.cpp
+++ server/MySQL.cpp
@@ -1,8 +1,10 @@
 MySQL::MySQL(Config* conf){
 	driver = get_driver_instance();
-	char* host = json_object_get_string(conf->get("database.MySQL.host"));
-	char* port = json_object_get_string(conf->get("database.MySQL.port"));
-	char* conn = new char[7+strlen(host)+strlen(port)+1];
+	const char* host = json_object_get_string(conf->get("database.MySQL.host"));
+	const char* port = json_object_get_string(conf->get("database.MySQL.port"));
+	int strLen = 7+strlen(host)+strlen(port)+1;
+	char* conn = new char[strLen];
+	snprintf(conn, strLen, "tcp://%s:%s", host, port);
 	strncpy(strncpy(strncpy(strncpy(conn,"tcp://",6)+6,host,strlen(host))+strlen(host), ":",1)+1, port, strlen(port)); // tcp://host:port
 	con = driver->connect(conn, json_object_get_string(conf->get("database.MySQL.user")), json_object_get_string(conf->get("database.MySQL.user")));
 	con->setSchema(json_object_get_string(conf->get("database.MySQL.dbname")));
