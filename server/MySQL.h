424e8395e53c9f9e58e3a7590b1436e66e348db7
Allen Poon <a1b2c3d42003hongkong@gmail.com>
2014-11-19 21:18:05 +0800

    fixed bug

--- server/MySQL.h
+++ server/MySQL.h
@@ -1,10 +1,13 @@
 #include <json/json.h>
 #include <cstring>
 
-#include <cppconn/driver.h>
-#include <cppconn/exception.h>
-#include <cppconn/resultset.h>
-#include <cppconn/statement.h>
+#include <mysql_connection.h>
+#include <mysql_driver.h>
+
+//#include <cppconn/driver.h>
+//#include <cppconn/exception.h>
+//#include <cppconn/resultset.h>
+//#include <cppconn/statement.h>
 
 #ifndef MySQL_H
 #define MySQL_H
