a80cec1dfcdd3c635bb3a3e5827b0fe67c5cafb8
Allen Poon <a1b2c3d42003hongkong@gmail.com>
2015-02-28 21:55:38 +0800

    fixed bug

--- server/Config.h
+++ server/Config.h
@@ -13,15 +13,16 @@ class Config{
 		static json_object *root, *nul;
 
 	private:
-		static char tmpStr[CONFIG_MAX_KEY+1]; //config file key name should not exceed 20 char
+		static char *tmpStr[CONFIG_MAX_KEY+1]; //config file key name should not exceed 20 char
 	public:
 		static void init();
 		static json_object *getFrom(const char *path, json_object *curRoot);
 		static json_object *get(const char *path);
 };
 
-static json_object *Config::root = NULL;
-static json_object *Config::nul = NULL;
+json_object *Config::root = NULL;
+json_object *Config::nul = NULL;
+char *Config::tmpStr = (char*) malloc(CONFIG_MAX_KEY+1);
 
 #include "Config.cpp"
 
