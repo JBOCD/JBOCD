c4020ca89ad70a16bbab679919f64c58a09f1bb4
pcl1401 <pcl1401@proj62.cse.cuhk.edu.hk>
2015-02-28 22:46:25 +0800

    remove debug message

--- server/main.cpp
+++ server/main.cpp
@@ -11,21 +11,16 @@
 using namespace std;
 
 int main() {
-	puts("hi");
-
 	Config::init();
-	puts("hi");
+
 	// init Memory Manager
 	MemManager::init();
-	puts("hi");
 
 	// init File Manager
 	FileManager::init();
-	puts("hi");
 
 	// init WebSocket
 	WebSocket::init();
-	puts("hi");
 
 	// start server
 	new Server();
